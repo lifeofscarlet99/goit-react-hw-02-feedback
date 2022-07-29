db7d677abdb09f69bc774808f6e3878715bd771c
Anastasia Slezko <slezkoanastasiia@gmail.com>
2022-07-29 11:55:07 +0300

    react/feedback
    react/feedbacl/completed

---
+++ src/components/App/App.jsx
@@ -0,0 +1,73 @@
+import { Component } from 'react';
+import FeedbackOptions from '../App/FeedbackOptions/FeedbackOptions';
+import Notification from '../App/Notification/Notification';
+import Statistics from '../App/Statistics/Statistics';
+import Section from '../App/Section/Section';
+import css from '../App/App.module.css';
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleClickButton = e => {
+    const option = e.target.name;
+
+    if (option) {
+      this.setState(prevState => ({ [option]: prevState[option] + 1 }));
+    }
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedback = () => {
+    const totalFeedback = this.countTotalFeedback();
+    const goodFeedback = this.state.good;
+    let result = 0;
+
+    if (totalFeedback > 0) {
+      result = Math.ceil((goodFeedback / totalFeedback) * 100);
+    }
+
+    return `${result}%`;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const countTotalFeedback = this.countTotalFeedback();
+    const countPositiveFeedbackPercentage = this.countPositiveFeedback();
+    const options = Object.keys(this.state);
+    const handleClickButton = this.handleClickButton;
+    return (
+      <div className={css.container}>
+        <div className={css.wrapper}>
+          <Section title="Please leave feedback">
+            <FeedbackOptions
+              options={options}
+              onLeaveFeedback={handleClickButton}
+            />
+          </Section>
+
+          <Section title="Statistics">
+            {countTotalFeedback > 0 ? (
+              <Statistics
+                good={good}
+                neutral={neutral}
+                bad={bad}
+                total={countTotalFeedback}
+                positivePercentage={countPositiveFeedbackPercentage}
+              />
+            ) : (
+              <Notification message="There is no feedback" />
+            )}
+          </Section>
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
